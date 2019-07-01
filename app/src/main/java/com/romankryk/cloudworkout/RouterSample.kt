ddbe2af3bc1ac2f0b826622e9f8938142365d723
Roman <romankryk1803@gmail.com>
2019-07-01 22:11:44 +0300

    New change

--- app/src/main/java/com/romankryk/cloudworkout/RouterSample.kt
+++ app/src/main/java/com/romankryk/cloudworkout/RouterSample.kt
@@ -2,7 +2,9 @@ package com.romankryk.cloudworkout
 
 import ru.terrakok.cicerone.BaseRouter
 import ru.terrakok.cicerone.Navigator
+import ru.terrakok.cicerone.Screen
 import ru.terrakok.cicerone.commands.BackTo
+import ru.terrakok.cicerone.commands.Forward
 
 class RouterSample : BaseRouter(){
     private var navigator: Navigator? = null
@@ -18,6 +20,7 @@ class RouterSample : BaseRouter(){
     fun newRootScreenWithMessage(screenKey: String, data: Any?, message: String){
         if (navigator != null) {
             navigator!!.applyCommands(arrayOf(BackTo(null)))
+            navigator!!.applyCommands(arrayOf(Forward('')))
         }
     }
 }
--- app/src/main/java/com/romankryk/cloudworkout/Screens.kt
+++ app/src/main/java/com/romankryk/cloudworkout/Screens.kt
@@ -2,19 +2,16 @@ package com.romankryk.cloudworkout
 
 import androidx.fragment.app.Fragment
 import com.romankryk.cloudworkout.fragments.Welcome
+import ru.terrakok.cicerone.Screen
 import ru.terrakok.cicerone.android.support.SupportAppScreen
 
 
 
 class Screens {
-    class SampleScreen(private val number: Int) : SupportAppScreen() {
+    public val main = object: Screen() {
+    }
 
-        init {
-            this.screenKey = javaClass.simpleName + "_" + number
-        }
-
-        override fun getFragment(): Fragment {
-            return Welcome.getNewInstance(number)
-        }
+    fun doit(){
+        main.screenKey
     }
 }
